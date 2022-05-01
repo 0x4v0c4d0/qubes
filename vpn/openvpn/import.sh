0ef09a8cc649a77dbdc037b6ca6cf769a766d5aa
0x4v0c4d0 <104268605+0x4v0c4d0@users.noreply.github.com>
2022-05-01 17:15:35 -0400

    Update import.sh

--- vpn/openvpn/import.sh
+++ vpn/openvpn/import.sh
@@ -11,15 +11,15 @@ PASS="$3" # Optional
 
 for f in $(ls -1 "${CONFIGS_PATH}/"*.ovpn)
 do    
-        sudo nmcli connection import type openvpn file $f
+        nmcli connection import type openvpn file $f
 	if [[ "${PASS}" == '' ]]; then
     		continue
   	fi
         name=`basename -s .ovpn $f`;
-        sudo nmcli connection modify "${name}" vpn.user-name "${USERNAME}"
-        sudo nmcli connection modify "${name}" vpn.secrets password="${PASS}"
-	sudo nmcli connection modify "${name}" +vpn.data password-flags=0
-	sudo nmcli connection modify "${name}" +vpn.data cert-pass-flags=0
+        nmcli connection modify "${name}" vpn.user-name "${USERNAME}"
+        nmcli connection modify "${name}" vpn.secrets password="${PASS}"
+	nmcli connection modify "${name}" +vpn.data password-flags=0
+	nmcli connection modify "${name}" +vpn.data cert-pass-flags=0
 	sudo echo '[vpn-secrets]' >> /etc/NetworkManager/system-connections/"${name}"
 	sudo echo password="${PASS}" >> /etc/NetworkManager/system-connections/"${name}"
 done
