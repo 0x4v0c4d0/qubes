7cf9f6ec0d495171823f2b64fb98df8ebb2a2440
0x4v0c4d0 <104268605+0x4v0c4d0@users.noreply.github.com>
2022-05-02 12:41:00 -0400

    Update vpn-template.sh

--- setup/vpn-template.sh
+++ setup/vpn-template.sh
@@ -2,15 +2,3 @@
 
 # Install OpenVPN client for NetworkManager
 sudo apt-get install openvpn network-manager-openvpn --yes
-
-# Import configs
-# Configs should be imported into vpn template qube
-sudo chmod +x /etc/myqubes/vpn/openvpn/import.sh
-sudo /etc/myqubes/vpn/openvpn/import.sh /etc/myqubes/vpn/openvpn/configs/free-vpngate
-
-# Import configs with credentials:
-#sudo /etc/myqubes/vpn/openvpn/import.sh /etc/myqubes/vpn/openvpn/configs/free-protonvpn username password
-
-# And you can import any other configs
-#sudo /etc/myqubes/vpn/openvpn/import.sh folder/with/ovpn/configs/ username password
-#sudo /etc/myqubes/vpn/openvpn/import.sh folder/with/ovpn/configs/
